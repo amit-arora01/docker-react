313567a3fe5138732f93ad283abbfa8d03d14ac4
Amit Arora <amit.arora01@gmail.com>
2020-12-25 15:06:26 +0530

    Renerated Project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This text loads automatically 
+          Learn React
         </a>
       </header>
     </div>
